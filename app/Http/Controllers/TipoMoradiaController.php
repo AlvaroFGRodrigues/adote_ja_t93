8e73efd9f9e69aa41c0e47db0b6f2ab864ae99a2
Alvaro Fernando <goncalvesf431@gmail.com>
2023-09-25 20:01:20 -0300

    Inicio de projeto

---
+++ app/Http/Controllers/TipoMoradiaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipoMoradia;
+use Illuminate\Http\Request;
+
+class TipoMoradiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipoMoradia $TipoMoradia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipoMoradia $TipoMoradia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TipoMoradia $TipoMoradia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipoMoradia $TipoMoradia)
+    {
+        //
+    }
+}
