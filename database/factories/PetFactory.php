b5024d457a311ad37e2adf11bcf670c5010a58be
felps47 <feaugustotimao@gmail.com>
2023-09-26 22:14:48 -0300

    começo da factorie que eu fiz errado

--- database/factories/PetFactory.php
+++ database/factories/PetFactory.php
@@ -1,23 +1,67 @@
 <?php
 
-namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pet>
- */
-class PetFactory extends Factory
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Database\Schema\Blueprint;
+
+use Illuminate\Support\Facades\Schema;
+
+
+
+return new class extends Migration
+
 {
+
     /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
+
+     * Run the migrations.
+
      */
-    public function definition(): array
+
+    public function up(): void
+
     {
-        return [
-            //
-        ];
+
+        Schema::create('tipos', function (Blueprint $table) {
+
+            $table->increments('id_pet');
+            $table->integer('id_porte');
+            $table->integer('id_genero');
+            $table->integer('id_tipo');
+            $table->integer('id_controle');
+            $table->string('nome_pet',45);
+            $table->string('idade');
+            $table->string('descricao',45);
+            $table->string('vacinas',45);
+            $table->string('racao',45);
+            $table->string('historico',45);
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            // lll fasfasfasfasfasf
+
+        });
+
     }
-}
+
+
+
+    /**
+
+     * Reverse the migrations.
+
+     */
+
+    public function down(): void
+
+    {
+
+        Schema::dropIfExists('tipos');
+
+    }
+
+};
+
