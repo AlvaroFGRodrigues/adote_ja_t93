d17d49ad0d585711b15a47e2ac6f7852e7645c13
felps47 <feaugustotimao@gmail.com>
2023-10-05 20:03:39 -0300

    aaa

---
+++ resources/views/layouts/pet/form.blade.php
@@ -0,0 +1,35 @@
+<form
+    action="{{
+    $funcionario
+    ?
+    route('funcionario.update',['id'=>$funcionario->id_funcionario])
+    :
+    route('funcionario.store')
+    }}
+    "
+    method="post" enctype="multipart/form-data" class="row g-3">
+        @csrf
+        <div class="col-md-6">
+            <label for="funcioanrio_custo" class="form-label">dono*</label>
+            <input class="form-control"
+            type="text"
+            id="funcionario"
+            name="funcionario"
+            value="{{
+            $funcionario ?
+            $funcionario->funcionario_custo  :
+            old('funcionario')
+            }}"
+            required>
+        </div>
+
+        <div class="col-md-2">
+            <input class="btn btn-primary mt-4" type="submit"
+        value="{{$funcionario ?
+            'Atualizar' :
+            'Cadastrar'
+            }}">
+        </div>
+
+
+    </form>
