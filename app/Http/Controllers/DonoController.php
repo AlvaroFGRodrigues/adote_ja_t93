31067803112a5ab9d97dc69b82914eee07cd74a6
Alvaro Fernando <goncalvesf431@gmail.com>
2023-10-10 22:03:46 -0300

    AlteracaoDono

--- app/Http/Controllers/DonoController.php
+++ app/Http/Controllers/DonoController.php
@@ -14,11 +14,41 @@ use Illuminate\Http\Request;
 
 class DonoController extends Controller
 {
-    public function index()
+     public function index(Request $request)
     {
-        $donos = Donos::orderBy('nome')->paginate(10);
-        return view('donos.index')
-            ->with(compact('donos'));
+        $search = $request->get('search');
+        $id_dono = $request->get('id_dono');
+        $id_status = $request->get('id_status');
+
+        $donos = Donos::with(['DonoHasResidencia', 'status','adocao'])
+            ->where(function ($query) use (
+                $search,
+                $id_dono,
+                $id_status
+            ) {
+
+                if ($search) {
+                    $query->orwhere('nome','like' , "%$search%");
+                    $query->orwhere('id_dono',  $search);
+                }
+                if ($id_dono) {
+                    $query->orwhere('id_dono', $id_dono);
+                }
+                if ($id_status) {
+                    $query->orwhere('id_status', $id_status);
+                }
+
+            })->orderBy('id_status', 'desc')
+                 ->paginate(40)
+                ;
+
+                $id_status = id_status::class;
+
+                return view('Donos.index')
+                ->with(compact(
+                    'status',
+                    'id_status',
+                ));
     }
 
     public function create()
